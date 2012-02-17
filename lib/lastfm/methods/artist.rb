85a0270c717a0732193914e98f2f80c8ca48f97a
Shane McDonald <anatomyofashane@gmail.com>
2012-02-16 21:17:45 -0500

    whoops

--- lib/lastfm/methods/artist.rb
+++ lib/lastfm/methods/artist.rb
@@ -8,7 +8,7 @@ module Lastfm
     class << self
 
       def build_url(method, artist)
-        Lastfm::Request.get(method, "artist")
+        Lastfm::Request.get(method, artist)
       end
 
       def get_similar(artist)

364bcae5a10e950e232f52041803126decb87fa8
Shane McDonald <anatomyofashane@gmail.com>
2012-02-20 14:22:12 -0500

    figured out method generation

--- lib/lastfm/base.rb
+++ lib/lastfm/base.rb
@@ -1,11 +1,21 @@
 module Lastfm
   class Base
 
-    # Generate methods that dont require auth
-    def methods_without_auth(*attrs)
+    attr_accessor :attrs
+    alias :to_hash :attrs
+
+    ## Method generation
+    # Lastfm::Artist.method("Dr. Dog")
+    def self.artist_methods_without_auth(*attrs)
       attrs.each do |attr|
-        
+        define_singleton_method attr do |artist|
+          build_url "artist.#{attr}", artist
+        end
       end
+    end
+
+    def initialize(attrs={})
+      @attrs = attrs.dup
     end
 
   end
--- lib/lastfm/methods/artist.rb
+++ lib/lastfm/methods/artist.rb
@@ -5,14 +5,12 @@ module Lastfm
   class Artist < Lastfm::Base
     include Lastfm::Request
 
+    artist_methods_without_auth :getsimilar, :getinfo, :getshouts, :gettags, :gettoptags
+
     class << self
 
       def build_url(method, artist)
         Lastfm::Request.get(method, artist)
-      end
-
-      def get_similar(artist)
-        build_url "artist.getsimilar", artist
       end
 
     end
